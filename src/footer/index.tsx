3897a57c7e542c879ea058f02a38560421c85e51
Danilo SOARES <fdanilosoares@gmail.com>
2021-01-07 20:48:19 -0300

    add footer

---
+++ src/footer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { ReactComponent as InstaIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+
+import './styles.css';
+
+const Footer = () => (
+  <footer className="main-footer">
+    App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+    <div className="footer-icons">
+      <a href="youtube" target="_new">
+        <YoutubeIcon />
+      </a>
+      <a href="linkeedin" target="_new">
+        <LinkedinIcon />
+      </a>
+      <a href="instagram" target="_new">
+        <InstaIcon />
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
